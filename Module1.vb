bd80b87b6ab1bc2c641fcc3c51332cae52d55d3c
Shad Miller <sdm12@sage.southhills.edu>
2010-10-19 15:23:21 -0400

    New commit

---
+++ Module1.vb
@@ -0,0 +1,35 @@
+﻿Imports System.Xml
+
+Module Module1
+
+    Sub Main()
+
+        Try
+            'Retrieve XML document   
+            Dim reader As XmlTextReader = New XmlTextReader("http://weather.yahooapis.com/forecastrss?w=2353991")
+
+            reader.Read()
+
+            'Finds the tag 
+            reader.ReadToFollowing("yweather:condition")
+
+            'Finds and displays attribute
+            Console.WriteLine(reader.GetAttribute("temp"))
+
+        Catch ex As InvalidCastException
+            Console.WriteLine("Invalid Data")
+
+        Catch ex As Exception
+            Console.WriteLine("Error occurred")
+
+        End Try
+
+
+    End Sub
+
+
+
+
+
+
+End Module
